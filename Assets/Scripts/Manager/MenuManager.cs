9ec06c1ec8599c6a3493e2b3d26b4549958dbdff
Omra <>
2026-08-30 02:02:16 -0400

    Main menu Ui

--- Assets/Scripts/Manager/MenuManager.cs
+++ Assets/Scripts/Manager/MenuManager.cs
@@ -4,9 +4,20 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
+    [SerializeField] private GameObject uiCreditElement;
+    [SerializeField] private GameObject uiTutoElement;
     public void Next()
     {
         SceneManager.LoadScene("GrandmaWolf");
     }
 
+    public void ToggleCreditElementUI()
+    {
+        uiCreditElement.SetActive(!uiCreditElement.activeSelf);
+    }
+
+    public void ToggleTutoElementUI()
+    {
+        uiTutoElement.SetActive(!uiTutoElement.activeSelf);
+    }
 }
